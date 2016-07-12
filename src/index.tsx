1a3ba49f6a15294ac2cbf95549e42f2ed7ba4f02
Kenley Bastari <kenleykb@gmail.com>
2016-07-12 17:25:08 +1000

    attempted to add typescript

---
+++ src/index.tsx
@@ -0,0 +1,8 @@
+import * React from "react";
+import * as ReactDOM from "react-dom";
+
+import { Hello } from  "./Components/Hello";
+
+ReactDOM.render(<Hello compiler="TypeScript" framework="React" />
+document.getElementById("example"));
+    
